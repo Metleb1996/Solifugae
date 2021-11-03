dd1aa11b3b4bb9191c1009477ad5c2d162af2d26
Metleb1996 <metlebrustemov1996@gmail.com>
2021-11-03 22:41:37 +0400

    First day

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
+import os
 from core.collector import walker, parser, writer
+from core.constants import header
+
+
+all_links = []
+i = 0
+
+def get_all_links(url, header, a):
+    a+=1
+    if a > 4:
+        return
+    links = walker(url, header=header)
+    for link in links:
+        if not link in all_links:
+            all_links.append(link)
+            text, name = parser(link, header)
+            name = os.path.join(os.getcwd(), "articles/"+name+".txt")
+            if writer(text, name):
+                print(a, " ))  ", link, "          Added&Created  all:", len(all_links))
+        print(a, " ))  ", link)
+    for link in links:
+            get_all_links(link, header, a=a)
+    a-=1
+
+
+if __name__ == '__main__':
+    get_all_links("https://adpuquba.edu.az/ilham-heyd%c9%99r-oglu-%c9%99liyevin-ilk-d%c9%99f%c9%99-prezident-secilm%c9%99sind%c9%99n-18-il-otur/", header=header, a=i)
+
+        
--- core/collector/functions.py
+++ core/collector/functions.py
@@ -1,8 +1,28 @@
-def walker():
-    pass
+import os
+import requests
+from bs4 import BeautifulSoup
 
-def parser():
-    pass
+def walker(url, header):
+    page = requests.get(url, headers=header)
+    page = BeautifulSoup(page.content, "html5lib") #lxml, html5, html5lib
+    links_div = page.find("div", attrs={"id":"recent-posts-2"})
+    links_ = links_div.find("ul").find_all("li")
+    links = []
+    for li in links_:
+        a = li.find("a")
+        links.append(a["href"])
+    return links
 
-def writer():
-    pass
+def parser(url:str, header):
+    page = requests.get(url, headers=header)
+    page = BeautifulSoup(page.content, "html5lib") #lxml, html5, html5lib
+    article_text = page.find_all("div", attrs={"class":"entry-content"})
+    slug = url.rsplit('/')[-2]
+    return article_text[0].text, slug
+
+def writer(text, name):
+    if not os.path.exists(name):
+        with open(name, "w") as f:
+            f.write(text)
+        return True
+    return False
