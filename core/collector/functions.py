d9713227f21a454adec8272019a3a7ecb5cd943b
Metleb1996 <metlebrustemov1996@gmail.com>
2021-11-08 11:06:09 +0400

    Added adpu_q_ prefix from  function names

--- core/collector/functions.py
+++ core/collector/functions.py
@@ -2,7 +2,9 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-def walker(url, header):
+all_links = []
+
+def adpu_q_walker(url, header):
     page = requests.get(url, headers=header)
     page = BeautifulSoup(page.content, "html5lib") #lxml, html5, html5lib
     links_div = page.find("div", attrs={"id":"recent-posts-2"})
@@ -13,7 +15,7 @@ def walker(url, header):
         links.append(a["href"])
     return links
 
-def parser(url:str, header):
+def adpu_q_parser(url:str, header):
     page = requests.get(url, headers=header)
     page = BeautifulSoup(page.content, "html5lib") #lxml, html5, html5lib
     article_text = page.find_all("div", attrs={"class":"entry-content"})
@@ -26,3 +28,21 @@ def writer(text, name):
             f.write(text)
         return True
     return False
+
+def adpu_q_get_all_links(url, header, limit, a = 0):
+    a+=1
+    if a > limit:
+        return
+    links = adpu_q_walker(url, header=header)
+    for link in links:
+        if not link in all_links:
+            all_links.append(link)
+            text, name = adpu_q_parser(link, header)
+            name = os.path.join(os.getcwd(), "articles/"+name+".txt")
+            if writer(text, name):
+                print( a, " ))  ", link, "          Added&Created  all:", len(all_links))
+        print(a, " ))  ", link)
+    for link in links:
+            adpu_q_get_all_links(link, header, limit=limit, a=a)
+    a-=1
+    return True
