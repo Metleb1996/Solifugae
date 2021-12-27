77fc062282196ead70281b9cac51af548df06f93
Metleb1996 <metlebrustemov1996@gmail.com>
2021-12-27 16:21:17 +0400

    Little edit

--- app.py
+++ app.py
@@ -1,26 +1,52 @@
 import os
-import click
+import sys
 from core.collector import adpu_q_get_all_links
+from core.parser import parsePhrases, parseWords
 from core.constants import header, ARTICLES_DIR
 
-@click.command()
-@click.option("--step", prompt="Step", help="collect -> Collect Articles      merge -> Merge Articles") 
-@click.option("--url", default="https://adpuquba.edu.az/ilham-heyd%c9%99r-oglu-%c9%99liyevin-ilk-d%c9%99f%c9%99-prezident-secilm%c9%99sind%c9%99n-18-il-otur/", prompt="Url", help="Url unvanini daxil edin") 
-def main(step, url):
+phrases = set()
+words = set()
+
+def merge():
+    with open(os.getcwd()+ARTICLES_DIR+"merged.txt", "w") as merged_f:
+        for file in os.listdir(os.getcwd()+ARTICLES_DIR):
+            with open(os.getcwd()+ARTICLES_DIR+file, "r") as _f:
+                text = _f.read()
+            merged_f.write(text)
+
+def main(step, url, parse):
+    text = ''
     if step == "collect":
         adpu_q_get_all_links(url=url, header=header, limit=1)
     elif step == "merge":
-        text = ''
-        with open(os.getcwd()+ARTICLES_DIR+"merged.txt", "w") as merged_f:
-            for file in os.listdir(os.getcwd()+ARTICLES_DIR):
-                with open(os.getcwd()+ARTICLES_DIR+file, "r") as _f:
-                    text = _f.read()
-                merged_f.write(text)
+        merge()
+    elif step == "parse":
+        with open(os.getcwd()+ARTICLES_DIR+"merged.txt", "r") as text_f:
+            text = text_f.read()
+        if parse == "phrases":
+            phrases.update(parsePhrases(text))
+        elif parse == "words":
+            words.update(parseWords(text)) 
     else:
         return False
 
 
 if __name__ == '__main__':
-    main()
+    step = "collect"
+    url = "https://adpuquba.edu.az/ilham-heyd%c9%99r-oglu-%c9%99liyevin-ilk-d%c9%99f%c9%99-prezident-secilm%c9%99sind%c9%99n-18-il-otur/"
+    parse = "phrases"
+    if len(sys.argv)>1:
+        for arg in sys.argv:
+            if arg == "--step":
+                step = sys.argv[sys.argv.index(arg)+1]
+            if arg == "--url":
+                url = sys.argv[sys.argv.index(arg)+1]
+            if arg == "--parse":
+                parse = sys.argv[sys.argv.index(arg)+1]
+    main(step, url, parse)
+    for p in phrases:
+        print(p)
+    for w in words:
+        print(w)
 
         
